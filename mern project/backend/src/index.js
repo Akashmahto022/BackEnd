24f9414a7d38911f726aabc7cccae0db6fea44c8
Akash kumar <akashmahto1169@gmail.com>
2024-03-01 21:36:08 +0530

    setup backend with express and mongoDB

--- mern project/backend/src/index.js
+++ mern project/backend/src/index.js
@@ -1,18 +1,21 @@
 // require('dotenv').config({path: './env'});
-import dotenv from 'dotenv'
-import connectDB from './db/index.js'
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
 connectDB()
-
-
-
-
-
-
+  .then(() => {
+    app.listen(process.env.PORT || 4000, (req, res) => {
+      console.log(`server is runing at port: ${process.env.PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log("mongoDB connection failed !!! ", error);
+  });
 
 // import { express } from "express";
 
