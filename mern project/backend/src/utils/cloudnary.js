f792722660f56fc834f004111cf8dc2cc73b2866
Akash kumar <akashmahto1169@gmail.com>
2024-03-10 18:03:51 +0530

    added cloudnary for file upload

---
+++ mern project/backend/src/utils/cloudnary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+          
+cloudinary.config({ 
+  cloud_name: 'djio2ihvr', 
+  api_key: '573466484821246', 
+  api_secret: 'NojpUEwF8Tm4nvsk_npK5U3t6U0' 
+});
