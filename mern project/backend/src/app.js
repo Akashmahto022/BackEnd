e5ce9255f7eeb3b9ef3442c40df31efe293684cc
Akash kumar <akashmahto1169@gmail.com>
2024-03-16 20:16:24 +0530

    understanding router and controller with debugging and use postman for api testing

--- mern project/backend/src/app.js
+++ mern project/backend/src/app.js
@@ -28,4 +28,15 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+
+//routes import 
+import userRouter from "./routes/user.routes.js";
+
+
+
+//routes declaration
+
+app.use("/api/v1/users", userRouter)
+
+// https://localhost:4000/api/v1/users/register
 export { app };
--- mern project/backend/src/utils/asyncHandler.js
+++ mern project/backend/src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler)=>{
-    (req, res, next)=>{
+    return (req, res, next)=>{
         Promise
         .resolve(
             requestHandler(req, res, next)
@@ -7,8 +7,6 @@ const asyncHandler = (requestHandler)=>{
         .catch((err)=>next(err))
     }
 }
-
-
 export {asyncHandler}
     
 
