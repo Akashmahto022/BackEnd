561e91aead4b7145375a570c1c7a5f11d99478f7
Akash kumar <akashmahto1169@gmail.com>
2024-04-02 18:06:06 +0530

    added refresh token

--- mern project/backend/src/controllers/user.controller.js
+++ mern project/backend/src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { user } from "../models/user.model.js";
 import { uploadOnCloundnary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken"
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -177,4 +178,48 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User Loged Out"));
 });
 
-export { registerUser, loginUser, logoutUser };
+const refreshAccessToken = asyncHandler(async (req, res)=>{
+  const incomingRefreshToekn = req.cookies.refreshToken || req.body.refreshToken
+
+  if(!incomingRefreshToekn){
+    throw new ApiError(401, "Unauthorized request")
+  }
+
+  try {
+    const decodedToekn = jwt.verify(incomingRefreshToekn, process.env.REFRESH_TOKEN_SECRET)
+  
+    const userId = await user.findById(decodedToekn?._id)
+  
+    if (!userId) {
+      throw new ApiError(401, "invalid refresh token")
+    }
+  
+    if (incomingRefreshToekn !== userId?.refreshToken) {
+      throw new ApiError(401, "refresh token is expired or used")
+    }
+  
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(userId._id)
+    return res
+    .status(201)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", newRefreshToken, options)
+    .json(
+      new ApiResponse(
+        200, 
+        {accessToken, refreshToken: newRefreshToken},
+        "Access and refresh token refreshed"
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error?.message || "invalid refresh token")
+  }
+
+
+
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken};
