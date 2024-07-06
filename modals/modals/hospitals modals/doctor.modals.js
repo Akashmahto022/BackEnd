00ba2ea7b22d3c117fb95a801a19e2f3d3cc2c3d
Akashmahto022 <akashmahto1169@gmail.com>
2024-07-06 14:07:40 +0530

    complete modals

--- modals/modals/hospitals modals/doctor.modals.js
+++ modals/modals/hospitals modals/doctor.modals.js
@@ -17,7 +17,13 @@ const doctoreSchema = new mongoose.Schema(
         experienceInYears: {
             type:Number,
             default: 0
-        }
+        },
+        worksInHospitals: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Hospital"
+            }
+        ]
     }, {timestamps:true})
 
 export const Doctor = mongoose.model("Doctor", doctoreSchema)
