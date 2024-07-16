c119909ca0d3c3f70c731704c38db24ff27607e8
Akashmahto022 <akashmahto1169@gmail.com>
2024-07-16 17:23:30 +0530

    added routes for user register

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
-import express from express;
+import express from "express";
 import cors from 'cors';
 import cookieparser from 'cookie-parser'
-
+import router from './routes/user.routes.js';
 
 const app = express()
 
@@ -14,5 +14,8 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: '16kn'}))
 app.use(express.static("public"))
 app.use(cookieparser())
+
+// routes declaration
+app.use('/api/v1/user', router)
 
 export { app }
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import {asyncHandler} from '../utils/asyncHandler.js'
+
+const registerUser = asyncHandler(async(req, res)=>{
+    res.status(200).json({
+        success:true,
+        message: "ok Akash mahto"
+    })
+})
+
+
+export {registerUser}
